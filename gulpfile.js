c12cc725bb028c6cd14ddda72637ed75c0a1192c
Roy Daniels <roy@nextfinity.net>
2014-03-07 02:34:49 -0700

    Add static server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,22 @@
 var gulp  = require('gulp'),
   jshint  = require('gulp-jshint'),
   component = require('gulp-component'),
-  livereload = require('gulp-livereload');
+  livereload = require('gulp-livereload'),
+  dest = 'build';
+
+gulp.task('staticsvr', function(next) {
+  var staticS = require('node-static'),
+      server = new staticS.Server('./'),
+      port = 8001;
+  require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+      server.serve(request, response);
+    }).resume();
+  }).listen(port, function() {
+    console.log('\nServer listening on port: ' + port + '\n');
+    next();
+  });
+});
 
 gulp.task('default', ['lint'], function () {
   return gulp.src('component.json')
@@ -17,10 +32,10 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['staticsvr'], function () {
   var server = livereload();
   gulp.watch(['component.json', 'src/**/*'], ['default']);
-  gulp.watch('build/**').on('change', function(file) {
+  gulp.watch(dest + '/**').on('change', function(file) {
     server.changed(file.path);
   });
 });
